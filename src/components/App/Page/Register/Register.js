43d0e1e8f3443f0a9ab2e765d7cb3cf999687966
chiennguyen99 <duychien226@gmail.com>
2019-06-20 12:49:00 +0700

    chiencommit

---
+++ src/components/App/Page/Register/Register.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'; 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
+import AppBar from 'material-ui/AppBar'; 
+import RaisedButton from 'material-ui/RaisedButton'; 
+import TextField from 'material-ui/TextField'; 
+import axios from 'axios'; 
+
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.State = {
+            user_name: '',
+            email: '',  
+            password: '', 
+        } 
+    }
+
+    handleClick(event) {
+        console.log("Success"); 
+    }
+
+    render() {
+        return (
+            <div>
+
+                <MuiThemeProvider>
+                    <div>
+                        <AppBar title="Register" />
+                        <TextField 
+                            hintText = "Enter your User Name"
+                            floatingLabelText = "User Name"
+                            onChange = {(event, newValue) => this.setState({user_name:newValue})}
+                        />
+                        <br />
+                        <TextField 
+                            hintText = "Enter your Email"
+                            floatingLabelText = "Your Email"
+                            onChange = {(event, newValue) => this.setState({email:newValue})}
+                        />
+                        <br />
+                        <TextField
+                            type = "password"
+                            hintText="Enter your Password"
+                            floatingLabelText="Password"
+                            onChange = {(event,newValue) => this.setState({password:newValue})}
+                        />
+                        <br />
+                        <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+                    </div>
+                    
+                </MuiThemeProvider>
+
+            </div>
+        ); 
+    }
+
+}
+
+const style = {
+    margin: 15,
+};
+
+export default Register;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Register from './components/App/Page/Register/Register.js'; 
+import * as serviceWorker from './serviceWorker';
+
+
+ReactDOM.render(<Register />, document.getElementById('Register')); 
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
